163cca8b64f1f079b0cd8b8731bac3f2d37b7452
Ryan H <rhurtis@hotmail.com>
2020-07-04 18:29:09 +0000

    Add more conditional examples.

--- conditionals.js
+++ conditionals.js
@@ -12,7 +12,7 @@ if (cold) {
 
 console.log("Now you're ready to go outside!");*/
 
-const temperature=300;
+/* const temperature=300;
 
 if (temperature < 0) {
   console.log("Make sure you pick out a scarf!");
@@ -22,4 +22,32 @@ if (temperature < 0) {
   console.log("Short sleeves are fine.");
 }
 
-console.log("Now you're ready to go outside!");
+console.log("Now you're ready to go outside!"); */
+
+//------------------------------ Logical Operators---------------------------------------------------------
+
+// Example using AND.
+/*
+const isCitizen = true;
+const age = 26;
+
+if (isCitizen && age>18) {
+  console.log("You are eligible to vote.");
+}
+*/
+
+// Example using OR. 
+/*
+let temperature=9001;
+if (temperature < -40 || temperature > 40) {
+  console.log("Maybe going outside isn't such a great idea…");
+}
+*/
+
+// Example using NOT.
+
+let raining=false;
+
+if (!raining){
+  console.log("Leave your umbrella at home!")
+}
