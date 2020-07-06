4ae36e855e426e5d252b935a4c974bd29d6c1fae
Ryan H <rhurtis@hotmail.com>
2020-07-06 18:33:40 +0000

    Basic Functions

---
+++ hello.js
@@ -0,0 +1,20 @@
+// Compass: Declaring and Calling Functions
+
+// Function that prints Hello, world when called.
+/*
+const sayHello  = function () {
+  console.log("Hello, world");
+}
+
+sayHello();
+
+
+*/
+
+// Improved version of the function above. 
+
+const sayHello  = function (name) {
+  console.log("Hello, " + name);
+}
+
+sayHello('Ryan');
